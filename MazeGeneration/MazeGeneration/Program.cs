7bff9616183a6780b2741ccb415c791e4a8cfa74
henrisin <henri.sinokki@gmail.com>
2014-11-05 12:59:15 +0200

    Main
    Conflicts:
    	MazeGeneration/MazeGeneration.v12.suo
    	MazeGeneration/MazeGeneration/Program.cs
    	MazeGeneration/MazeGeneration/bin/Debug/MazeGeneration.exe
    	MazeGeneration/MazeGeneration/bin/Debug/MazeGeneration.pdb
    
    	MazeGeneration/MazeGeneration/obj/Debug/MazeGeneration.csprojResolveAssemblyReference.cache
    	MazeGeneration/MazeGeneration/obj/Debug/MazeGeneration.exe
    	MazeGeneration/MazeGeneration/obj/Debug/MazeGeneration.pdb

--- MazeGeneration/MazeGeneration/Program.cs
+++ MazeGeneration/MazeGeneration/Program.cs
@@ -12,9 +12,61 @@ namespace MazeGeneration
     {
         static void Main(string[] args)
         {
-            
+            char option, option2 = 'x';
+            bool loop = true;
+            int seed = 0, height = 0, width = 0;
             //TODO WELCOME MESSAGE - INFO
 
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("****                           LABYRINT CREATOR                             ****");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("                          Welcome to Labyrint creator 0.5");
+            Console.WriteLine("                              Press E to continue...");
+            option = (char)Console.Read();
+            Console.ReadLine();
+            while (option == 'E')
+            {
+                Console.Clear();
+
+                // LABYRINT DETAILS
+                Console.WriteLine("Enter a seed value:");
+                seed = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter a height of the labyrint:");
+                height = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter a width of the labyrint:");
+                width = int.Parse(Console.ReadLine());
+
+                // LABYRINT PRINT
+                Map map = new Map(width, height, seed);
+                map.printMap();
+
+
+                // NEW LABYRINT
+                while(loop)
+                { 
+                    Console.WriteLine("Press N to create a new labyrinth or Q to quit");
+                    option2 =(char) Console.Read();
+                    if (option2 == 'Q')
+                    {
+                        option = option2;
+                        loop = false;
+                    }
+                    else if (option2 == 'N')
+                    {
+                        loop = false;
+                    }
+                    else
+                    {
+                        Console.Write("Wrong value, please try again.");
+                        loop = true;
+                    } 
+               }
+                loop = true;
+            }
+
+            
             //TODO LOOP WHILE EXIT COMMAND
             //TODO ASK FOR SEED, MAPSIZE, MAZE COMPLEXITY AND HORIZONTAL INFLUENCE.
  
@@ -30,12 +82,12 @@ namespace MazeGeneration
             }*/
             
 
+<<<<<<< HEAD
+=======
             Map map = new Map(75, 45, 1339);
             map.printMap();
+>>>>>>> origin/master
 
-            
-            Console.ReadLine();
-            Console.ReadLine();
              
         }
     }
